18d4a9613bff2875b0eb8dfab1b7aa48473d9e11
Yaroslav Lebedenko <scram109@gmail.com>
2022-01-25 10:34:11 +0200

    Регистрация пользователя

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -13,5 +14,11 @@ db = SQLAlchemy(app)
 
 # обьект представляющий механизм миграции
 migrate = Migrate(app, db)
+
+# инициализация Flask-логин
+login = LoginManager(app)
+
+# Значение 'login' это имя функции (или конечной точки) для входа в систему. Другими словами, имя, которое вы будете использовать в url_for() вызове для получения URL-адреса.
+login.login_view = 'login'
 
 from app import views, models
--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # класс User наследуется от db.Model базового класса для всех моделей
-class User(db.Model):
+class User(UserMixin, db.Model):
     # Поля создаются как экземпляры db.Column класса, который принимает тип поля в качестве аргумента, а также другие необязательные аргументы, которые, например, позволяют указать, какие поля являются уникальными и проиндексированы, что важно для эффективного поиска в базе данных.
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -10,17 +12,30 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-    # Метод __repr__ сообщает Python, как печатать объекты этого класса
+    # метод __repr__ сообщает Python, как печатать объекты этого класса
     def __repr__ (self):
-      return '<User {}>'.format(self.user)
+        return '<User {}>'.format(self.user)
+
+    # хэширование и проверка пароля.
+    def set_password(self, password):
+      self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+# Пользовательский загрузчик регистрируется в Flask-Login с помощью декоратора @login.user_loader.
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 # класс Post представлет сообщения в блогах, написанные пользователями.
-class Post():
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__ (self):
-      return '<Post {}>'.format(self.body)
+        return '<Post {}>'.format(self.body)
