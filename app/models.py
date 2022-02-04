b593877bb2833c717d2f60448701db1d5b2e94dd
Yaroslav Lebedenko <scram109@gmail.com>
2022-02-04 15:59:30 +0200

    Уведомления пользователя

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+import json
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
 
@@ -79,6 +80,20 @@ class User(UserMixin, db.Model):
         backref=db.backref('followers', lazy='dynamic'),
         lazy='dynamic'
     )
+    messages_sent = db.relationship(
+        'Message', 
+        foreign_keys='Message.sender_id',
+        backref='author', 
+        lazy='dynamic'
+    )
+    messages_received = db.relationship(
+        'Message',
+        foreign_keys='Message.recipient_id',
+        backref='recipient', 
+        lazy='dynamic'
+    )
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship( 'Notification', backref='user', lazy='dynamic')
 
     # метод __repr__ сообщает Python, как печатать объекты этого класса
     def __repr__(self):
@@ -132,6 +147,20 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    # количество непрочитаных сообщений у пользователя
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
+
+    # добавляем уведомление для пользователя в базу данных если
+    # уведомление с таким именем уже существует, оно будет удалено
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -149,3 +178,25 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
